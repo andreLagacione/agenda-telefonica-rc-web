614b24de6f99f9b2117b239226fc7ef43695b8f8
AndreLagacione <lagacioneandre@gmail.com>
2021-02-22 23:00:54 -0300

    creat input mask and validate

---
+++ src/base/form/form.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Form = styled.div`
+    
+`;
+
+export default Form;
--- src/pages/contatos/create.tsx
+++ src/pages/contatos/create.tsx
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from '../../base/form/form.styles';
+import { phoneMask } from '../../base/masks/phone';
+import TitleComponent from '../../base/title/title';
+import { phoneValidator } from '../../base/validators/phone';
 
 const CreateContact: React.FC = () => {
+    const [nome, setNome] = useState<string>('');
+    const [telefone, setTelefone] = useState<string>('');
+    const [isPhoneValid, setIsPhoneValid] = useState<boolean>();
+    let title = 'Criar contato';
+
+    const getPhoneMask = (value: string) => {
+        const masked = phoneMask(value);
+        setTelefone(masked);
+        validatePhone();
+    }
+
+    const validatePhone = () => {
+        const isValid = phoneValidator(telefone);
+        setIsPhoneValid(isValid);
+    }
+
+    const saveContact = () => {
+        console.log(nome, telefone);
+    }
+
     return (
-        <h1>Criar contatos</h1>
+        <>
+            <TitleComponent title={title} />
+
+            <Form className="p-3 alert alert-dark mt-4">
+                <form>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <label className="form-label d-block mb-2">Telefone *</label>
+                            <input
+                                type="tel"
+                                name="telefone"
+                                className={ isPhoneValid === true ? 'form-control' : 'form-control border-danger' }
+                                placeholder="(99) 9 9999-9999"
+                                maxLength={16}
+                                value={telefone}
+                                onChange={(e: any) => getPhoneMask(e.target.value)}
+                                onBlur={() => validatePhone()}
+                            />
+                        </div>
+
+                        <div className="col-sm-6">
+                            <label className="form-label d-block mb-2">Nome</label>
+                            <input
+                                type="text"
+                                name="nome"
+                                className="form-control"
+                                value={nome}
+                                onChange={e => setNome(e.target.value)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="d-flex justify-content-end mt-3">
+                        <button type="button" className="btn btn-secondary">Voltar</button>
+                        <button
+                            type="button"
+                            className="btn btn-primary ml-3"
+                            onClick={() => saveContact()}
+                            disabled={!isPhoneValid}
+                        >Salvar</button>
+                    </div>
+                </form>
+            </Form>
+        </>
     );
 }
 
